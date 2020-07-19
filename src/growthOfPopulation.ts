d5dcdb1830f800f9bd786c6a33d5e0cbe81c2f66
Fadi Serapian <fadi.serapian@gmail.com>
2020-07-18 20:50:49 -0400

    add growthOfPopulation

---
+++ src/growthOfPopulation.ts
@@ -0,0 +1,34 @@
+/******************** GROWTH OF POPULATION **********************/
+/*
+  In a small town the population is p0 = 1000 at the beginning of a year.
+  The population regularly increases by 2 percent per year
+  and moreover 50 new inhabitants per year come to live in the town.
+  How many years does the town need to see its population greater or equal
+  to p = 1200 inhabitants?
+
+  Example:
+    At the end of the first year there will be:
+    1000 + 1000 * 0.02 + 50 => 1070 inhabitants
+
+    At the end of the second year there will be:
+    1070 + 1070 * 0.02 + 50 => 1141 inhabitants
+
+    At the end of the third year there will be:
+    1141 + 1141 * 0.02 + 50 => 1213
+
+    It will need 3 entire years.
+*/
+
+function nbYears(p0: number, percent: number, aug: number, p: number): number {
+  let count = 0;
+
+  while (p0 < p) {
+    p0 = p0 + p0 * percent / 100 + aug;
+
+    count++;
+  }
+
+  return count;
+}
+
+console.log(nbYears(1000, 2, 50, 1200));
