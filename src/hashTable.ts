79c4c33b31f7188af14e365b774d6675e2c340fd
Fadi Serapian <fadi.serapian@gmail.com>
2023-02-04 15:04:24 -0500

    Add keys() method

--- src/hashTable.ts
+++ src/hashTable.ts
@@ -47,12 +47,31 @@ class HashTable {
 
         return undefined;
     }
+
+    public keys() {
+        if (!this.data.length) {
+            return undefined;
+        }
+
+        const keyArray = [];
+        for (let i = 0; i < this.data.length; i++) {
+            const item = this.data[i];
+
+            if (item) {
+                keyArray.push(item[0][0]);
+            }
+        }
+
+        return keyArray;
+    }
 }
 
-const myHashTable = new HashTable(2);
+const myHashTable = new HashTable(50);
 
 myHashTable.set('grapes', 1000);
 myHashTable.set('fruit', 2000);
 myHashTable.set('banana', 3000);
 
 console.log(myHashTable.get('grapes')); // 1000
+
+console.log(myHashTable.keys());
